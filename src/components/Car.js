ecfff5468642b2d71baaec1d822b7d8d47ff1330
Veronika050987 <veronicaviv@mail.ru>
2026-05-17 13:53:51 +0300

    bus as bus done

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Car.css';
-import CarSVG from './Car.svg';
 
 const Car = ({position, rotation}) => {
     return(
@@ -11,13 +10,39 @@ const Car = ({position, rotation}) => {
             top:`${position.y}px`,
             transform:`rotate(${rotation}deg)`,
             // Центрируем поворот относительно центра машинки
-            transformOrigin:'50% 50%',
+            //transformOrigin:'50% 50%',
         }}        
         >
-               {/* Простая визуализация машинки */}
-                {/* Можно заменить на SVG или img */}   
-        </div>
-    );
+              {/* Колеса */}
+      <div className="wheel front-left-wheel"></div>
+      <div className="wheel front-right-wheel"></div>
+      <div className="wheel rear-left-wheel"></div>
+      <div className="wheel rear-right-wheel"></div>
+
+      {/* Двери */}
+      <div className="door-left"></div>
+      <div className="door-right"></div>
+
+      {/* Фары (если нужны) */}
+      {/* <div className="headlight-left"></div> */}
+      {/* <div className="headlight-right"></div> */}
+
+      {/* Задние фонари */}
+      <div className="taillight-left"></div>
+      <div className="taillight-right"></div>
+
+      {/* Глаза */}
+      <div className="eye-left">
+        <div className="pupil"></div>
+      </div>
+      <div className="eye-right">
+        <div className="pupil"></div>
+      </div>
+
+      {/* Рот */}
+      <div className="mouth"></div>
+    </div>
+  );
 };
 
 export default Car;
